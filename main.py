8c971b044d69ff6310c709e0a1ab5bf17262e014
Frederik Baerentsen <drudoo@drudoo.com>
2022-06-07 21:59:03 +0200

    fixed zip issue (#1)

--- main.py
+++ main.py
@@ -45,34 +45,35 @@ def import2sql():
         for file in files:
             f = os.path.join(root, file)
             #try:
-            try:
-                s = zipfile.ZipFile(f)
-                #s = gzip.GzipFile(f)
-                Bs_data = BeautifulSoup(s.open('ComicInfo.xml').read(), "xml")
-                #print(Bs_data.select('Series')[0].text, file=sys.stderr)
-                #print(Bs_data.select('Title')[0].text, file=sys.stderr)
-                CVDB=re.findall('(?<=\[CVDB)(.*)(?=].)', Bs_data.select('Notes')[0].text)
-                #list.append('CVDB'+CVDB[0] + ': '  + Bs_data.select('Series')[0].text + "(" + Bs_data.select('Volume')[0].text + ") : " + Bs_data.select('Number')[0].text  )
-                #print(list, file=sys.stdout)
-            
-                ISSUE=Bs_data.select('Number')[0].text
-                SERIES=Bs_data.select('Series')[0].text
-                VOLUME=Bs_data.select('Volume')[0].text
-                PUBLISHER=Bs_data.select('Publisher')[0].text
+            if f.endswith('.cbz'):
                 try:
-                    TITLE=Bs_data.select('Title')[0].text
+                    s = zipfile.ZipFile(f)
+                    #s = gzip.GzipFile(f)
+                    Bs_data = BeautifulSoup(s.open('ComicInfo.xml').read(), "xml")
+                    #print(Bs_data.select('Series')[0].text, file=sys.stderr)
+                    #print(Bs_data.select('Title')[0].text, file=sys.stderr)
+                    CVDB=re.findall('(?<=\[CVDB)(.*)(?=].)', Bs_data.select('Notes')[0].text)
+                    #list.append('CVDB'+CVDB[0] + ': '  + Bs_data.select('Series')[0].text + "(" + Bs_data.select('Volume')[0].text + ") : " + Bs_data.select('Number')[0].text  )
+                    #print(list, file=sys.stdout)
+            
+                    ISSUE=Bs_data.select('Number')[0].text
+                    SERIES=Bs_data.select('Series')[0].text
+                    VOLUME=Bs_data.select('Volume')[0].text
+                    PUBLISHER=Bs_data.select('Publisher')[0].text
+                    try:
+                        TITLE=Bs_data.select('Title')[0].text
+                    except:
+                        TITLE="" #sometimes title is blank.
+                    PATH=f 
+                    UPDATED=str(datetime.datetime.now())
+                    #print(UPDATED,file=sys.stdout)
+                    #sql="INSERT OR REPLACE INTO COMICS (CVDB,ISSUE,SERIES,VOLUME, PUBLISHER, TITLE, FILE,PATH,UPDATED) VALUES ("+CVDB[0]+",'"+ISSUE+"','"+SERIES+"','"+VOLUME+"','"+PUBLISHER+"','"+TITLE+"','"+file+"','" + f + "','" + UPDATED + "')"
+                    #print(sql,file=sys.stdout)
+                    #conn.execute(sql);
+                    conn.execute("INSERT OR REPLACE INTO COMICS (CVDB,ISSUE,SERIES,VOLUME, PUBLISHER, TITLE, FILE,PATH,UPDATED) VALUES (?,?,?,?,?,?,?,?,?)", (CVDB[0], ISSUE, SERIES, VOLUME, PUBLISHER, TITLE, file, f, UPDATED))
+                    conn.commit()
                 except:
-                    TITLE="" #sometimes title is blank.
-                PATH=f 
-                UPDATED=str(datetime.datetime.now())
-                #print(UPDATED,file=sys.stdout)
-                #sql="INSERT OR REPLACE INTO COMICS (CVDB,ISSUE,SERIES,VOLUME, PUBLISHER, TITLE, FILE,PATH,UPDATED) VALUES ("+CVDB[0]+",'"+ISSUE+"','"+SERIES+"','"+VOLUME+"','"+PUBLISHER+"','"+TITLE+"','"+file+"','" + f + "','" + UPDATED + "')"
-                #print(sql,file=sys.stdout)
-                #conn.execute(sql);
-                conn.execute("INSERT OR REPLACE INTO COMICS (CVDB,ISSUE,SERIES,VOLUME, PUBLISHER, TITLE, FILE,PATH,UPDATED) VALUES (?,?,?,?,?,?,?,?,?)", (CVDB[0], ISSUE, SERIES, VOLUME, PUBLISHER, TITLE, file, f, UPDATED))
-                conn.commit()
-            except:
-                print(f,file=sys.stdout)
+                    print(f,file=sys.stdout)
     
     conn.close()
     elapsed = timeit.default_timer() - start_time

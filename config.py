59256c35ac79237906cce69bb952ca3a71f61878
FrederikBaerentsen <frederik+gitea@baerentsen.net>
2023-01-17 16:17:22 +0100

    Added windows library location

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 from werkzeug.security import generate_password_hash
 
-#CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/library")
-CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/home/drudoo/ComicsTest/Comics")
+#CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/library") #docker
+#CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/home/drudoo/ComicsTest/Comics") #linux
+CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/Comics/ComicRack") #windows
+
 
 TEENYOPDS_ADMIN_PASSWORD = os.getenv("TEENYOPDS_ADMIN_PASSWORD", None)
 users = {}

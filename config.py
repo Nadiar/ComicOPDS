f6acbfa3e4dbbe499a5b55201fe235a4fb25d8fa
FrederikBaerentsen <frederik+gitea@baerentsen.net>
2023-01-19 22:20:51 +0100

    added thumbnails dir and default search limit

--- config.py
+++ config.py
@@ -4,9 +4,18 @@ from werkzeug.security import generate_password_hash
 #CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/library") #docker
 #CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/home/drudoo/ComicsTest/Comics") #linux
 CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "/Comics/ComicRack") #windows
+#CONTENT_BASE_DIR = os.getenv("CONTENT_BASE_DIR", "testlibrary") #windows test library
+
+THUMBNAIL_DIR = os.getenv("THUMBNAIL_DIR",'thumbnails')
+
 WIN_DRIVE_LETTER = 'B'
 DEFAULT_SEARCH_NUMBER = 10
 
+DEBUG = True
+
+def _print(arg):
+    if DEBUG:
+        print(arg)
 
 TEENYOPDS_ADMIN_PASSWORD = os.getenv("TEENYOPDS_ADMIN_PASSWORD", None)
 users = {}
